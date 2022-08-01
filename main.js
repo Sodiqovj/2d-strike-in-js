514138a59956f290071166bcf0a7dc895c0febd4
Sodiqovj <holmirzaevsohruh196@gmail.com>
2022-08-01 15:31:37 +0500

    main

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let attackKnifeBtn = document.querySelector('.attackKnifeBtn')
 let battleSound = document.querySelector('.battle-sound')
 let knifeSound = document.querySelector('.knife-sound')
 let startSound = document.querySelector('.start-sound')
+let new_bullet = document.querySelector('.new-bullet')
 let bombSound = document.querySelector('.bomb-sound')
 let character = document.querySelector('.character')
 let attackBtn = document.querySelector('.attackBtn')
@@ -32,6 +33,7 @@ character.style.transition = '0s !important'
 
 
 // controls character position
+
 window.addEventListener('keyup', function(event){
     switch (event.key) {
         case "ArrowLeft":
@@ -49,7 +51,7 @@ window.addEventListener('keyup', function(event){
 // checking Status
 let ckeckingStatus = setInterval(() => {
     if(gameStart==true){
-        battleSound.play()
+        // battleSound.play()
              // stand if 
             if (characterStatus=='stand') {
                 character.src = characterStill
@@ -77,9 +79,11 @@ attackBtn.addEventListener('click', function(){
         turn = false
         if (characterStatus=='left') {
             bullet.classList.add('bulletLeft')
+            new_bullet.classList.add('new-bullet-left')
         }
         else if (characterStatus=='right') {
             bullet.classList.add('bulletRight')
+            new_bullet.classList.add('new-bullet-right')
         }
         else{
             bullet.classList.add('bulletRight')
@@ -88,9 +92,16 @@ attackBtn.addEventListener('click', function(){
             turn = true
             bullet.classList.remove('bulletLeft')
             bullet.classList.remove('bulletRight')
-        }, 100);
+            new_bullet.classList.remove('new-bullet-left')
+            new_bullet.classList.remove('new-bullet-right')
+        }, 1000);
     }
 })
+
+function new_bullet_reset(){
+    bullet.classList.remove('bulletLeft')
+    bullet.classList.remove('bulletRight')
+}
 
 let loading = setInterval(() => {
             if (load==true) {
@@ -106,7 +117,7 @@ let loading = setInterval(() => {
             }, 1000);
 }, 1);
 
-let createDonre =  setInterval(() => {
+let createDrone =  setInterval(() => {
     if(turnDron==true&&gameStart==true){
         setTimeout(() => {
             turnDron = false
@@ -125,58 +136,84 @@ let createDonre =  setInterval(() => {
         drone.classList.add('right')
         drone.classList.add('droneGo')
     }
-}, 5000);
+}, 3000);
 
-let deadDrone = setInterval(() => {
+
+
+let dronesFunc = setInterval(() => {
     let drones = document.querySelectorAll('.droneGo')
-    drones.forEach(function(item){
-        let itemLeft = parseInt(window.getComputedStyle(item).getPropertyValue('left'))
-        let itemRight = parseInt(window.getComputedStyle(item).getPropertyValue('right'))
-        drones.forEach(function(item2){
-            let itemLeft2 = parseInt(window.getComputedStyle(item2).getPropertyValue('right'))
-            let itemRight2 = parseInt(window.getComputedStyle(item2).getPropertyValue('right'))
-            if (turn==false&&itemLeft<itemLeft2&&characterStatus=='left') {
-                item.classList.add('dead')
-                setTimeout(() => {
-                    item.remove()
-                }, 2000);
+    drones.forEach(drone => {
+        let new_bullet_left = parseInt(window.getComputedStyle(new_bullet).getPropertyValue('left'))
+        // let droneLeft = parseInt(window.getComputedStyle(drone).getPropertyValue('left'))
+        if (turn==false) {
+            if (new_bullet_left<drone.style.left&&drone.classList == 'left droneGo'&&characterStatus=='left') {
+                alert('left')
             }
-            if (turn==false&&itemRight<itemRight2&&characterStatus=='right') {
-                item.classList.add('dead')
-                setTimeout(() => {
-                    item.remove()
-                }, 2000);
+            if (new_bullet_left<drone.style.left&&drone.classList == 'right droneGo'&&characterStatus=='right') {
+                alert('right')
             }
-        })
-    })
+            console.log(drones[drone.ariaChecked]);
+        }
+    });
 }, 100);
 
-if (item.classList=='dead') {
-        item.style.backgroundImage = "url('images/bomb/bomb-1.png')"
-        setTimeout(() => {
-            if (item.classList=='dead') {
-                item.style.backgroundImage = "url('images/bomb/bomb-1.png')"
-                setTimeout(() => {
-                    item.style.backgroundImage = "url('images/bomb/bomb-1.png')"
-                }, 100);
-                setTimeout(() => {
-                    item.style.backgroundImage = "url('images/bomb/bomb-2.png')"
-                }, 300);
-                setTimeout(() => {
-                    item.style.backgroundImage = "url('images/bomb/bomb-3.png')"
-                }, 700);
-                setTimeout(() => {
-                    item.style.backgroundImage = "url('images/bomb/bomb-4.png')"
-                }, 900);
-                setTimeout(() => {
-                    item.style.backgroundImage = "url('images/bomb/bomb-5.png')"
-                }, 1300);
-                setTimeout(() => {
-                    item.style.backgroundImage = "url('images/bomb/bomb-6.png')"
-                }, 170);
-        }
-    }, 100);
-}
+
+
+
+
+
+
+
+
+// let deadDrone = setInterval(() => {
+//     let drones = document.querySelectorAll('.droneGo')
+//     drones.forEach(function(item){
+//         let itemLeft = parseInt(window.getComputedStyle(item).getPropertyValue('left'))
+//         let itemRight = parseInt(window.getComputedStyle(item).getPropertyValue('right'))
+//         drones.forEach(function(item2){
+//             let itemLeft2 = parseInt(window.getComputedStyle(item2).getPropertyValue('right'))
+//             let itemRight2 = parseInt(window.getComputedStyle(item2).getPropertyValue('right'))
+//             if (turn==false&&itemLeft>itemLeft2&&characterStatus=='left') {
+//                 item.classList.add('dead')
+//                 setTimeout(() => {
+//                     item.remove()
+//                 }, 2000);
+//             }
+//             if (turn==false&&itemRight>itemRight2&&characterStatus=='right') {
+//                 item.classList.add('dead')
+//                 setTimeout(() => {
+//                     item.remove()
+//                 }, 2000);
+//             }
+//             text.textContent = `item: ${itemLeft}, item2: ${itemLeft2}`
+//         })
+//     })
+// }, 100);
+
+
+
+
+
+// if (item.classList=='dead') {
+//                 setTimeout(() => {
+//                     item.style.backgroundImage = "url('images/bomb/bomb-1.png')"
+//                 }, 100);
+//                 setTimeout(() => {
+//                     item.style.backgroundImage = "url('images/bomb/bomb-2.png')"
+//                 }, 400);
+//                 setTimeout(() => {
+//                     item.style.backgroundImage = "url('images/bomb/bomb-3.png')"
+//                 }, 800);
+//                 setTimeout(() => {
+//                     item.style.backgroundImage = "url('images/bomb/bomb-4.png')"
+//                 }, 12000);
+//                 setTimeout(() => {
+//                     item.style.backgroundImage = "url('images/bomb/bomb-5.png')"
+//                 }, 1600);
+//                 setTimeout(() => {
+//                     item.style.backgroundImage = "url('images/bomb/bomb-6.png')"
+//                 }, 1900);
+// }
 
 
 
